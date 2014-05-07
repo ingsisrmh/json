41ffec45e32beebbc4b0c24f4c5edd4277a5db2f
Ricardo Mendoza Huesca <ingsisrmh@gmail.com>
2014-05-07 16:01:01 -0500

    Create procesar.php

---
+++ procesar.php
@@ -0,0 +1,11 @@
+<?php
+
+$cliente = array
+(
+	'codigo' => '0001',
+	'nombre' => 'García Robles, Alberto',
+	'correo' => 'correo@hotmai.com, alberto@yahoo.com, agarcia@gmail.com'
+);
+
+echo json_encode($cliente);
+?>
